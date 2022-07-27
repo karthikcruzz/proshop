eae6d37a9779efa2abeca437f0660cd446380123
Kalipatnapu Karthikeya Sarma <kkarthikmi@gmail.com>
2022-07-27 13:12:17 +0530

    Seeder.js file

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    _id: '1',
+    
     name: 'Airpods Wireless Bluetooth Headphones',
     image: '/images/airpods.jpg',
     description:
@@ -13,7 +13,7 @@ const products = [
     numReviews: 4,
   },
   {
-    _id: '2',
+    
     name: 'iPhone 11 Pro 256GB Memory',
     image: '/images/phone.jpg',
     description:
@@ -26,7 +26,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '3',
+   
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
@@ -39,8 +39,7 @@ const products = [
     numReviews: 6,
   },
   {
-    _id: '4',
-    name: 'Sony Playstation 4 Pro White Version',
+     name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
@@ -52,8 +51,7 @@ const products = [
     numReviews: 3,
   },
   {
-    _id: '5',
-    name: 'Logitech G-Series Gaming Mouse',
+     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
       'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
@@ -65,7 +63,6 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: '6',
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
